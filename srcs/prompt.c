98582616c8d2d80e5ddc871626a84df1a373b4c8
Lucas DUTRIEZ <ldutriez@e2r11p9.42.fr>
2020-03-02 20:06:27 +0100

    Add of a parsing :D

--- srcs/prompt.c
+++ srcs/prompt.c
@@ -6,7 +6,7 @@
 /*   By: ldutriez <ldutriez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/25 14:52:27 by ldutriez          #+#    #+#             */
-/*   Updated: 2020/03/02 11:32:52 by tguilbar         ###   ########.fr       */
+/*   Updated: 2020/03/02 20:04:00 by ldutriez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,27 +36,59 @@ static char	*get_usr(char **p_env)
 	return (ft_strdup("unknow"));
 }
 
-void		treatment(char *p_str)
+static void		*find_command(char *p_str)
 {
 	if (ft_strnstr(p_str, "echo", ft_strlen("echo")))
-		mini_echo(p_str + 5);
+		return (&mini_echo); //param
 	else if (ft_strnstr(p_str, "pwd", ft_strlen("pwd")))
-		mini_pwd();
+		return (&mini_pwd);
 	else if (ft_strnstr(p_str, "env", ft_strlen("env")))
-		mini_env();
+		return (&mini_env);
 	else if (ft_strnstr(p_str, "export", ft_strlen("export")))
-		mini_export(p_str + 7);
+		return (&mini_export);//param
 	else if (ft_strnstr(p_str, "unset", ft_strlen("unset")))
-		mini_unset(p_str + 6);
+		return (&mini_unset);//param
 	else if (ft_strnstr(p_str, "cd", ft_strlen("cd")))
-		mini_cd(p_str + 3);
+		return (&mini_cd);//param
 	else if (ft_strnstr(p_str, "exit", ft_strlen("exit")))
-		mini_exit();
+		return (&mini_exit);//param
 	else
 	{
 		ft_putstr(p_str);
 		ft_putstr(": command not found\n");
+		return (NULL);
 	}
+}
+
+static char **check_param(char *param)
+{
+	void 	**result;
+	size_t	index;
+	size_t	start;
+
+	result = ft_tab_new(0);
+	index = 0;
+	start = 0;
+	while (param[index])
+	{
+		if (param[index] == ' ')
+		{
+			ft_add_to_tab((void*)ft_strsub(param, start, index - start), &result);
+			start = index + 1;
+		}
+		else if (param[index] == '"')
+		{
+			index++;
+			while (param[index] != '"')
+				index++;
+			ft_add_to_tab((void*)ft_strsub(param, start, index + 1 - start), &result);
+			start = index + 1;
+		}
+		else if (param[index + 1] == '\0')
+			ft_add_to_tab((void*)ft_strsub(param, start, index + 1 - start), &result);
+		index++;
+	}
+	return ((char**)result);
 }
 
 int			main(int ac __attribute__((unused)),
@@ -71,7 +103,8 @@ int			main(int ac __attribute__((unused)),
 	print_prompt(user);
 	while (get_next_line(0, &str))
 	{
-		treatment(str);
+		//find_command(str);
+		ft_print_str_tab("args : ", check_param(str));
 		free(str);
 		print_prompt(user);
 	}
