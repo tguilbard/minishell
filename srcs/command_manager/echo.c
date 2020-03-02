6c80d70aec6a6c8d143edb7b3ba526607afd7f35
Tanguy GUILBARD <tguilbar@e2r12p5.42.fr>
2020-03-02 15:42:32 +0100

    new import buildint echo

--- srcs/command_manager/echo.c
+++ srcs/command_manager/echo.c
@@ -6,14 +6,16 @@
 /*   By: ldutriez <ldutriez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/26 09:57:36 by ldutriez          #+#    #+#             */
-/*   Updated: 2020/02/26 21:34:20 by tguilbar         ###   ########.fr       */
+/*   Updated: 2020/03/02 15:42:13 by tguilbar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+extern t_char_list	g_env;
+
 void	mini_echo(char *p_str)
 {
-	ft_putstr(p_str);
-	ft_putchar('\n');
+	if (execve("/bin/echo", p_str, g_env.data) == -1)
+		perror("minishell");
 }
