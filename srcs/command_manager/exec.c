036b41b831ba062b2bf8429078b625c2e94dab7e
Tanguy GUILBARD <tguilbar@e2r12p5.42.fr>
2020-03-10 11:22:55 +0100

    ajout du kill du fils d exec

--- srcs/command_manager/exec.c
+++ srcs/command_manager/exec.c
@@ -6,7 +6,7 @@
 /*   By: tguilbar <tguilbar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/09 15:51:20 by tguilbar          #+#    #+#             */
-/*   Updated: 2020/03/09 15:59:49 by tguilbar         ###   ########.fr       */
+/*   Updated: 2020/03/10 11:20:07 by tguilbar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,8 @@ void	mini_exec(char **p_param)
 	if (pid == 0)
 	{
 		if (execve(p_param[0], p_param, g_env.data) == -1)
-			ft_putstr("invalide executable");
+			ft_putstr("invalide executable\n");
+		exit(0);
 	}
 	else
 		waitpid(pid, NULL, 0);
