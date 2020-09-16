007706be80da2d96f8ff55fb8276b3f3a9175c70
NotCampeur <lucas.dutriez@gmail.com>
2020-09-16 10:19:53 +0200

    Update the exit error detection

--- srcs/command_manager/exit.c
+++ srcs/command_manager/exit.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   exit.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: user42 <user42@student.42.fr>              +#+  +:+       +#+        */
+/*   By: ldutriez <ldutriez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/26 20:58:31 by ldutriez          #+#    #+#             */
-/*   Updated: 2020/09/15 16:52:00 by user42           ###   ########.fr       */
+/*   Updated: 2020/09/16 10:17:19 by ldutriez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,33 +17,25 @@ extern int g_env_fd[2];
 
 int	mini_exit(char **p_param)
 {
-	int i;
-
 	signal(SIGINT, SIG_DFL);
 	signal(SIGQUIT, SIG_DFL);
 	ft_putstr("exit\n", 1);
 	if (p_param == NULL || p_param[1] == NULL)
 		ft_putnbr(g_ret, g_env_fd[1]);
+	else if (ft_str_is_only_digit(p_param[1]) == false)
+	{
+		ft_putstr("exit: ", 2);
+		ft_putstr(p_param[1], 2);
+		ft_putstr(": numeric argument required\n", 2);
+		ft_putstr("2", g_env_fd[1]);
+	}
 	else if (p_param[2] != NULL)
 	{
-		ft_putstr("exit: too many argument\n", 2);
+		ft_putstr("exit: too many arguments\n", 2);
 		child_killer(1);
 	}
-	else
-	{
-		i = 0;
-		while (p_param[1][i] && ft_is_alpha(p_param[1][i]) == false)
-			i++;
-		if (p_param[1][i])
-		{
-			ft_putstr("exit: ", 2);
-			ft_putstr(p_param[1], 2);
-			ft_putstr(": numeric argument required\n", 2);
-			ft_putstr("2", g_env_fd[1]);
-		}
-		else
-			ft_putstr(p_param[1], g_env_fd[1]);
-	}
+	else if (ft_str_is_only_digit(p_param[1]) == true)
+		ft_putstr(p_param[1], g_env_fd[1]);
 	close(g_env_fd[1]);
 	exit(0);
 }
